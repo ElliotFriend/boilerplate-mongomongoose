5c222ff0a007007b9ffbb0a8bc42a7bebe4dff27
Elliot Voris <elliot@voris.me>
2021-07-15 16:18:57 -0500

    Chain Search Query Helpers to Narrow Search Results
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- myApp.js
+++ myApp.js
@@ -108,8 +108,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({name: 1, age: 0, favoriteFoods: 1})
+    .exec(function(err, data) {
+      if (err) console.error(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
